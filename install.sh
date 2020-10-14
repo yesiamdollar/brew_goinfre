93d9e4e2b4f667969b8790b526f3d263d5f88b84
Achraf Boutahri <aboutahr@e2r4p6.1337.ma>
2020-10-14 13:53:57 +0100

    finish

---
+++ install.sh
@@ -0,0 +1,12 @@
+brew > brew_check 2>&1
+check=$(cat brew_check |  wc -l | tr -d " ")
+if [ $check -lt 5 ]
+then
+	path_to="/goinfre/$(whoami)/"
+	echo 'export path_to="/goinfre/$(whoami)/"'
+	git clone https://github.com/Homebrew/brew $path_to/.brew
+	echo 'export PATH=$path_to/.brew/bin:$PATH' >> $HOME/.zshrc
+	source $HOME/.zshrc
+	brew help
+fi
+rm brew_check
